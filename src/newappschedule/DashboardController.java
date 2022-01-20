e7699a5c0b5b45a6bc2adc7179210ed23e6a51fc
bayu firmansyah <ubay.zip>
2022-01-20 19:44:43 +0700

    upload file

---
+++ src/newappschedule/DashboardController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newappschedule;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author bayu firmansyah
+ */
+public class DashboardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
