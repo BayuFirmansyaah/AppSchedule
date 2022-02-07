7831b727bc2f4808b2fcfc54d2d53acb818d8e2b
bayu firmansyah <ubay.zip>
2022-02-07 18:12:37 +0700

    clean and build

--- src/newappschedule/getDataJadwal.java
+++ src/newappschedule/getDataJadwal.java
@@ -10,9 +10,50 @@ package newappschedule;
  * @author bayu firmansyah
  */
 public class getDataJadwal {
+    
+    String hari,kelas,jam,kode;
 
     getDataJadwal(String queryKelas, String queryHari, String queryJam, String queryKode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          this.hari = queryHari;
+          this.kelas = queryKelas;
+          this.jam = queryJam;
+          this.kode = queryKode;
     }
     
+    public String getHari(){
+        return hari;
+    }
+   
+    public String getKelas(){
+        return kelas;
+    }
+    
+    public String getJam(){
+        return jam;
+    }
+    
+    public String getKode(){
+        return kode;
+    }
+    
+    
+    public void setHari(String _hari){
+        this.hari = _hari;
+    }
+    
+    public void setKelas(String _kelas){
+        this.kelas = _kelas;
+    }
+    
+    public void setJam(String _jam){
+        this.jam = _jam;
+    }
+    
+    
+    public void setKode(String _kode){
+        this.kode = _kode;
+    }
+   
+   
+   
 }
